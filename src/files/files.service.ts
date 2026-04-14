a93fe4ef268dc648911eb4fc07d097abe29052be
Hanzeel Villa <hanzeelvilla0@gmail.com>
2026-04-13 22:24:26 -0600

    Save file info in database

--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -1,11 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { StorageService } from '../common/storage/storage.service';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { File } from './entities/file.entity';
 @Injectable()
 export class FilesService {
-  constructor(private readonly storageService: StorageService) {}
+  constructor(
+    @InjectRepository(File)
+    private readonly fileRepository: Repository<File>,
+    private readonly storageService: StorageService,
+  ) {}
 
   async uploadFile(file: Express.Multer.File) {
-    return await this.storageService.uploadFile(file);
+    // upload file to r2
+    const { key, url } = await this.storageService.uploadFile(file);
+
+    // save file info to database
+    const newFile = this.fileRepository.create({
+      key,
+      url,
+    });
+
+    return await this.fileRepository.save(newFile);
   }
 
   async remove(id: string) {
